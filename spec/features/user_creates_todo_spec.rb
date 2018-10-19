1bdec6e1617e4bbb9e1577356461429a0845e244
João Fernandes <joaovitorfernandes2@gmail.com>
2018-10-19 19:33:55 -0300

    Allow a user to craete a todo

---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+
+feature 'Use creates todo' do
+  let(:title) { 'buy milk' }
+
+  scenario 'successfully' do
+    visit root_path
+
+    click_on 'Add todo'
+
+    fill_in 'Title', with: title
+    click_on 'Create todo'
+
+    expect(page).to have_css 'ul.todos li', text: title
+  end
+end
