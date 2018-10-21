f05b4b8f2ae365efadcd99d6898a986a81640e03
João Fernandes <joaovitorfernandes2@gmail.com>
2018-10-21 19:31:09 -0300

    Refactor application to be polymorphic

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -11,7 +11,17 @@ class ShoutsController < ApplicationController
   private
 
   def shout_params
-    params.require(:shout).permit(:body)
+    { content: content_from_params }
+  end
+
+  def content_from_params
+    TextShout.new(body: content_params)
+  end
+
+  def content_params
+    params.require(:shout)
+          .require(:content)
+          .require(:body)
   end
 
   def redirect_options_for(shout)
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,7 +1,7 @@
 class Shout < ApplicationRecord
   belongs_to :user
+  belongs_to :content, polymorphic: true
 
-  validates :body, presence: true, length: { in: 1..144 }
   validates_presence_of :user
 
   default_scope { order(created_at: :desc) }
