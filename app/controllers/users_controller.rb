47df914a3a1df28c1b727b8ee50124ac931a3683
João Fernandes <joaovitorfernandes2@gmail.com>
2018-10-21 15:44:15 -0300

    Allow users to view shouts and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < Clearance::UsersController
+  def show
+    @user = User.find(params[:id])
+    @shouts = @user.shouts
+  end
+
   def new
     @user = User.new
   end
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,9 +1,10 @@
 class Shout < ApplicationRecord
   belongs_to :user
 
-  validates :body,
-            presence: true,
-            length: { in: 1..144 }
-
+  validates :body, presence: true, length: { in: 1..144 }
   validates_presence_of :user
+
+  default_scope { order(created_at: :desc) }
+
+  delegate :username, to: :user
 end
