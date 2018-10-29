97ef1ea0ece05e11a37725855f747e861159e511
João Fernandes <joaovitorfernandes2@gmail.com>
2018-10-29 01:50:36 -0300

    Allow for searching on photo shouts

--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -4,11 +4,28 @@ class ShoutSearchQuery
   end
 
   def to_relation
-    Shout.
-      joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id::BIGINT = text_shouts.id").
-      where("text_shouts.body LIKE ?", "%#{term}%")
+    matching_shouts_for_text_shouts.or(matching_shouts_for_photo_shouts)
   end
 
-  private
+  # private
+
   attr_reader :term
+
+  def matching_shouts_for_text_shouts
+    Shout.where(content_type: "TextShout", content_id: matching_text_shouts.pluck(:id))
+  end
+
+  def matching_text_shouts
+    TextShout.where("body LIKE ?", "%#{term}%")
+  end
+
+  def matching_shouts_for_photo_shouts
+    Shout.where(content_type: "PhotoShout", content_id: matching_photo_shouts.pluck(:id))
+  end
+
+  def matching_photo_shouts
+    PhotoShout.
+      joins("LEFT JOIN active_storage_attachments AS attachments ON attachments.record_type = 'PhotoShout' AND attachments.record_id::BIGINT = photo_shouts.id").
+      joins("LEFT JOIN active_storage_blobs AS blobs ON blobs.id = attachments.blob_id WHERE blobs.filename LIKE '%#{term}%'")
+  end
 end
